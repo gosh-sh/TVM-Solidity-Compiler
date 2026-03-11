5f94607949ac0658df4df3e7e4c93cfe786a4ac0
sehor05 <>
2026-03-11 16:42:54 +0300

    fix ast

--- sold/src/lib.rs
+++ sold/src/lib.rs
@@ -365,17 +365,14 @@ pub fn build(args: Args) -> Status {
             .as_object()
             .ok_or_else(|| parse_error!())?;
 
-        let mut array = vec![];
-        for (_, val) in all {
-            let ast = val
-                .as_object()
-                .ok_or_else(|| parse_error!())?
-                .get("ast")
-                .ok_or_else(|| parse_error!())?;
-            array.push(ast.clone());
-        }
-        assert_eq!(array.len(), 1);
-        println!("{}", serde_json::to_string(&array[0])?);
+        let ast = all
+            .get(&res.0)
+            .ok_or_else(|| parse_error!())?
+            .as_object()
+            .ok_or_else(|| parse_error!())?
+            .get("ast")
+            .ok_or_else(|| parse_error!())?;
+        println!("{}", serde_json::to_string(ast)?);
         return Ok(());
     }
 
