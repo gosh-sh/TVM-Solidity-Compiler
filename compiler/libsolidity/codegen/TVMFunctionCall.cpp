ec97e639fb69bc05ff2e8dcf0e496f023ab2ed18
sehor05 <>
2026-05-20 21:00:04 +0300

    delete junk

--- compiler/libsolidity/codegen/TVMFunctionCall.cpp
+++ compiler/libsolidity/codegen/TVMFunctionCall.cpp
@@ -1983,7 +1983,7 @@ void FunctionCallCompiler::variantMethods(MemberAccess const& _node) {
 void FunctionCallCompiler::addressMethod() {
 	if (m_memberAccess->memberName() == "transfer") { // addr.transfer(...)
 		std::map<int, Expression const *> exprs;
-		std::map<int, std::string> constParams{{TvmConst::int_msg_info::ihr_disabled, "1"}, {TvmConst::int_msg_info::bounce, "1"}, {TvmConst::int_msg_info::dest_dapp_id, "0"}};
+		std::map<int, std::string> constParams{{TvmConst::int_msg_info::ihr_disabled, "1"}, {TvmConst::int_msg_info::bounce, "1"}};
 		std::function<void(int)> appendBody;
 		std::function<void()> pushSendrawmsgFlag;
 		std::function<void()> appendStateInit;
@@ -2049,7 +2049,6 @@ void FunctionCallCompiler::addressMethod() {
 						break;
 					case str2int("dest_dapp_id"):
 						exprs[TvmConst::int_msg_info::dest_dapp_id] = m_arguments[arg].get();
-						constParams.erase(TvmConst::int_msg_info::dest_dapp_id);
 						break;
 					case str2int("stateInit"):
 						setAppendStateInit(m_arguments[arg].get());
